4588a6e212c93bd7642d0608073914a0cadf8549
Ernesto905 <ernesto90643@gmail.com>
2024-06-24 13:46:11 -0700

    Added wrapper shell scripts for initiating our transfer learning job

---
+++ sagemaker-train/bert/fine_tune.sh
@@ -0,0 +1,4 @@
+#!/bin/bash 
+
+# Fine-tune your BERT model 
+composer sequence_classification.py yamls/finetuning/mosaic-bert-base-uncased.yaml
---
+++ sagemaker-train/bert/pre_train.sh
@@ -0,0 +1,4 @@
+#!/bin/bash 
+
+# This will pre-train a Mosaic BERT 
+composer main.py yamls/main/mosaic-bert-base-uncased.yaml
